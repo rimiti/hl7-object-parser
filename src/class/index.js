0e06dd2df6066829dc30c1b1c71d4cd1569b9ed4
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-06-22 11:33:49 +0200

    feat(class/index.js) wrapper created to use decoder class with specified config file

--- src/class/index.js
+++ src/class/index.js
@@ -1,139 +1,24 @@
-import hl7 from './hl7/index.js'
+import Decoder from './decoder'
 
-export default class Decoder {
+let configuration
 
-  constructor(message, config) {
-    this._message = message
-    this._config = config
-    this._decoder = this._setDynamicDecoder(config.format)
+export default {
+
+  /**
+   * @description Configure usage
+   * @param config
+   */
+  configure: (config) => configuration = config,
+
+  /**
+   * @description Decode message with configuration
+   * @param emitter
+   * @param recipient
+   * @returns {Generator}
+   */
+  decode: (message) => {
+    let decoder = new Decoder(message, configuration)
+    return decoder
   }
 
-  get patient_id() {
-    return this._decoder.patient_id
-  }
-
-  get patient_id_complete() {
-    return this._decoder.patient_id_complete
-  }
-
-  get pid_origin() {
-    return this._decoder.pid_origin
-  }
-
-  get siu_origin() {
-    return this._decoder.siu_origin
-  }
-
-  get firstname() {
-    return this._decoder.firstname
-  }
-
-  get lastname() {
-    return this._decoder.lastname
-  }
-
-  get birthdate() {
-    return this._decoder.birthdate
-  }
-
-  get firstphone() {
-    return this._decoder.firstphone
-  }
-
-  get email() {
-    return this._decoder.email
-  }
-
-  get secondphone() {
-    return this._decoder.secondphone
-  }
-
-  get gender() {
-    return this._decoder.gender
-  }
-
-  get common_name() {
-    return this._decoder.common_name
-  }
-
-  get address() {
-    return this._decoder.address
-  }
-
-  get city() {
-    return this._decoder.city
-  }
-
-  get cp() {
-    return this._decoder.cp
-  }
-
-  get appointment_id() {
-    return this._decoder.appointment_id
-  }
-
-  get origin() {
-    return this._decoder.origin
-  }
-
-  get intervention_type() {
-    return this._decoder.intervention_type
-  }
-
-  get length() {
-    return this._decoder.length
-  }
-
-  get date() {
-    return this._decoder.date
-  }
-
-  get rpps() {
-    return this._decoder.rpps
-  }
-
-  get doctor_firstname() {
-    return this._decoder.doctor_firstname
-  }
-
-  get doctor_lastname() {
-    return this._decoder.doctor_lastname
-  }
-
-  get status() {
-    return this._decoder.status
-  }
-
-  get comment() {
-    return this._decoder.comment
-  }
-
-  get rpps_finess() {
-    return this._decoder.rpps_finess
-  }
-
-  get message_type() {
-    return this._decoder.message_type
-  }
-
-  get message_type_detail() {
-    return this._decoder.message_type_detail
-  }
-
-  get mrg_patient_id() {
-    return this._decoder.mrg_patient_id
-  }
-
-  get mrg_origin() {
-    return this._decoder.mrg_origin
-  }
-
-  _setDynamicDecoder(format) {
-    let obj = (format === 'hl7') ? new hl7(this._message, this._config) : null
-    if (!obj) throw new Error(`Unknow format: ${format}`)
-    return obj
-  }
 }
-
-
-
