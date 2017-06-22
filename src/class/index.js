40ae3687ff5cc866557ad3b0e3d97f3318cca25e
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-06-22 14:41:50 +0200

    feat(class/index.js) load json hl7 config from object not file

--- src/class/index.js
+++ src/class/index.js
@@ -1,5 +1,4 @@
 import Decoder from './decoder'
-import fs from 'fs'
 
 let configuration
 
@@ -13,15 +12,11 @@ export default {
 
   /**
    * @description Decode message with configuration
-   * @param emitter
-   * @param recipient
+   * @param message
    * @returns {Generator}
    */
   decode: (message) => {
-    fs.readFile(configuration.mapping_file, 'utf8', (err, data) => {
-      if (err) throw new Error(e)
-      let decoder = new Decoder(message, JSON.parse(data))
-      return decoder
-    })
+    let decoder = new Decoder(message, configuration.mapping)
+    return decoder
   }
 }
