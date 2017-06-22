6f06a78f00dfc3a9db57bccfcfbf526594536b4b
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-06-22 14:44:00 +0200

    feat(test/adt/a04.js) a04 test created

---
+++ test/adt/a04.js
@@ -0,0 +1,24 @@
+import parser from '../../lib/parser'
+import chai from 'chai'
+chai.should()
+
+describe('ADT', () => {
+  it(`A04`, (done) => {
+    const a04 = `MSH|^~\&|mllp_http_proxy|proxy00-prodaz|mllp_http_proxypartenaire|proxy00-prodpartenaire|20160923155836||ADT^A04|154779|P|2.5.1|||||FRA|UTF-8\rEVN|ADT^A04|20160923155836\rPID|||123456^^^ODS^PI||DO BAIRRO^Dimitri^^^^^L||19920506|M|Nom usuel||Avenue des Champs-Élysées^^Paris^^75000||0100000000^^^dimitri.dobairro@clicrdv.com~0200000000\rPV1||U`
+    let obj = parser.decode(a04)
+    obj.message_type.should.be.equal(`adt`)
+    obj.message_type_detail.should.be.equal(`a04`)
+    obj.patient_id.should.be.equal(`123456`)
+    obj.pid_origin.should.be.equal(`ODS`)
+    obj.firstname.should.be.equal(`Dimitri`)
+    obj.lastname.should.be.equal(`DO BAIRRO`)
+    obj.birthdate.should.be.equal('19920506')
+    obj.gender.should.be.equal(`M`)
+    obj.common_name.should.be.equal(`Nom usuel`)
+    obj.address.should.be.equal(`Avenue des Champs-Élysées`)
+    obj.city.should.be.equal(`Paris`)
+    obj.cp.should.be.equal(`75000`)
+    obj.firstphone.should.be.equal(`0100000000^^^dimitri.dobairro@clicrdv.com`)
+    done()
+  })
+})
