41a8a6b5748a2da33cfe77d93bc95e8ad27c56ef
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-07-18 14:28:56 +0200

    feat(class/hl7-2.4/index.js) doesn't throw error if segment doesn't exist

--- src/class/hl7-2.4/index.js
+++ src/class/hl7-2.4/index.js
@@ -29,14 +29,16 @@ export default class hl7 {
   process() {
     let obj = {}
     for (let segment in this.config.mapping) {
+      let s = this.message.getSegment(segment.toUpperCase())
       for (let value of this.config.mapping[segment].values) {
         if (value.field) {
           try {
-            let s = this.message.getSegment(segment.toUpperCase())
-            let index1 = value.component[0]
-            let index2 = value.component[1]
-            let val = (s.getField(index1, 1).includes('^') || s.getField(index1, 2)) ? s.getField(index1, index2) : s.getComponent(index1, index2)
-            this._generateObject(obj, value.field, val)
+            if (s instanceof Object) {
+              let index1 = value.component[0]
+              let index2 = value.component[1]
+              let val = (s.getField(index1, 1).includes('^') || s.getField(index1, 2)) ? s.getField(index1, index2) : s.getComponent(index1, index2)
+              this._generateObject(obj, value.field, val)
+            }
           } catch (e) {
             logger.warn(`[com/dec] - error during fetching hl7 ${segment} segment with [${index1}, ${index2}] index (${err.message})`)
           }
@@ -47,11 +49,11 @@ export default class hl7 {
   }
 
   _generateObject(obj, property, value = '') {
-    let path = property.split('.')
-    for (var i = 0, tmp = obj; i < path.length - 1; i++) {
-      tmp = tmp[path[i]] = {}
+    let paths = property.split('.')
+    for (var i = 0, tmp = obj; i < paths.length - 1; i++) {
+      tmp = (tmp[paths[i]]) ? Object.assign(tmp[paths[i]], tmp[paths[i]]) : tmp[paths[i]] = {}
     }
-    tmp[path[i]] = value
+    tmp[paths[i]] = value
   }
 
 }
