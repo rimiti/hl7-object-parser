779e1f860a26cf10e8b873ab00e18501a081790c
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-07-24 17:36:34 +0200

    fix(test/siu/index.js) use right config file

--- test/siu/index.js
+++ test/siu/index.js
@@ -194,7 +194,7 @@ test(`S17 - Notification of appointment deletion`, t => {
 
 test(`S26 - Notification that patient did not show up for scheduled appointment`, t => {
   const s26 = `MSH|^~\\&|mllp_http_proxy|proxy00-prodaz|mllp_http_proxypartenaire|proxy00-prodpartenaire|20161231110000||SIU^S26|256660|P|2.5.1|||||FRA|UTF-8|\rSCH||49849903800^DimSolution||||100|||||^^30^20161231110000|||||10101041431^KAYSSIEH^BASSEL||||ODS|||||Booked|\rPID|||123456^^^ODS^^PI||DO BAIRRO^Dimitri^^^^^L||19920506|M|Nom usuel||Avenue des Champs-Élysées^^Paris^^75000^^^^^||0100000000^^^dimitri.dobairro@dimsolution.com^^^^~0200000000^^^^^^^|\rRGS|1\rAIG|1|||10101041431@750057689\rNTE|||My comment`
-  const obj = parser.decode(s26, s17Mapping)
+  const obj = parser.decode(s26, s26Mapping)
   t.is(obj.msh.message_datetime, '20161231110000')
   t.is(obj.msh.message_type, 'SIU')
   t.is(obj.msh.message_type_ref, 'S26')
