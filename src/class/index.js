9d6416f504d603e026de73e864dc2fbd7c34a499
Dimitri <contact@dimsolution.com>
2017-08-30 14:23:06 +0200

    feat(class/decoder.js) file renamed

--- src/class/index.js
+++ src/class/index.js
@@ -1,16 +1,30 @@
-import Decoder from './decoder'
+import hl7_2_4 from './hl7-2.4'
 
-export default {
+export default class Decoder {
 
-  /**
-   * @description Decode message with configuration
-   * @param message
-   * @param config
-   * @return {*}
-   */
-  decode: (message, config) => {
-    let decoder = new Decoder(message, config)
-    return decoder.decode()
+  constructor(message, config) {
+    this._message = message
+    this._config = config
+    this._decoder = this._setDynamicDecoder(config.format)
   }
 
+  /**
+   * @description Call process method from dynamic class
+   * @return {*}
+   */
+  decode() {
+    return this._decoder.process()
+  }
+
+  /**
+   * @description Instantiate dynamically right class
+   * @param format
+   * @return {*}
+   * @private
+   */
+  _setDynamicDecoder(format) {
+    let obj = (format === 'hl7-2.4') ? new hl7_2_4(this._message, this._config) : null
+    if (!obj) throw new Error(`Unknow format: ${format}`)
+    return obj
+  }
 }
